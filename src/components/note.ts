8b35d105a21528c9ed96113f260ac2b6860e4092
IG-22\user <bitvalser@gmail.com>
2018-08-07 11:23:09 +0300

    update2

--- src/components/note.ts
+++
@@ -1,6 +0,0 @@
-export class Note {
-  public header: string;
-  public text: string;
-  public image: any;
-  public map: any;
-}
--- src/pages/notes/mock.notes.ts
+++
@@ -1,8 +0,0 @@
-import { Note } from '../../components/note'
-
-export const NOTES: Note[] = [
-    {header: 'some head1', text: 'some text1', image: null, map: null},
-    {header: 'some head2', text: 'some text2', image: null, map: null},
-    {header: 'some head3', text: 'some text3', image: null, map: null},
-    {header: 'some head4', text: 'some text4', image: null, map: null}
-]
